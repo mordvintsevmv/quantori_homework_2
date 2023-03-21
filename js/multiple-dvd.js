951a08c343521215d860ee18514c5815d57bd9fc
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-22 00:54:53 +0400

    Feature: Creating Help Text

--- js/multiple-dvd.js
+++ js/multiple-dvd.js
@@ -12,8 +12,8 @@ const createRandomFilter = () => {
 }
 
 // Function for creating random animation speed and delay with CSS animations
-createRandomAnimation = () => {
-    let delay = 30*Math.floor(Math.random() * 10);
+const createRandomAnimation = () => {
+    let delay = 30 * Math.floor(Math.random() * 10);
 
     let speedX = Math.floor(Math.random() * 10)
     let speedY = Math.floor(Math.random() * 10)
@@ -27,12 +27,13 @@ const addAnimation = (element) => {
 
     element.style.animation = createRandomAnimation();
 
-    element.addEventListener("animationiteration", ()=>{
+    element.addEventListener("animationiteration", () => {
         element.style.filter = createRandomFilter()
 
-        laser.cloneNode(true).play().then(() => {
-            document.getElementById('warning-text').style.display = "none"
-        });
+        laser.cloneNode(true).play()
+            .then(() => {
+                document.getElementById('warning-text').style.display = "none"
+            });
     })
     return element
 }
@@ -41,15 +42,18 @@ const addAnimation = (element) => {
 addAnimation(document.getElementById(`dvd-logo`))
 
 // Creating listener for adding new logos
-document.getElementById("dvd-add").addEventListener('click', ()=>{
+document.getElementById("dvd-add").addEventListener('click', () => {
 
+    // Enabling remove button
     document.getElementById("dvd-remove").style.display = "block"
 
+    // Checking if there is more then 6 logos
     const dvd_logos = document.getElementsByClassName('dvd-logo')
-    if (dvd_logos.length === 6){
+    if (dvd_logos.length > 6) {
         document.getElementById("dvd-add").style.display = "none"
     }
 
+    // Creating new logo
     let dvd_logo = document.createElement("img");
 
     dvd_logo.src = "../img/dvd-logo.png"
@@ -57,23 +61,34 @@ document.getElementById("dvd-add").addEventListener('click', ()=>{
     dvd_logo.style.filter = createRandomFilter();
     dvd_logo.alt = "dvd logo"
 
+    // Adding animations
     dvd_logo = addAnimation(dvd_logo)
 
     document.getElementById("screen-wrapper").appendChild(dvd_logo)
 })
 
 // Creating listener for removing last logo
-document.getElementById("dvd-remove").addEventListener('click', ()=>{
+document.getElementById("dvd-remove").addEventListener('click', () => {
     const dvd_logos = document.getElementsByClassName('dvd-logo')
 
+    // Enabling add button
     document.getElementById("dvd-add").style.display = "block"
-    if (dvd_logos.length === 2){
+
+    // Removing last logo from page
+    dvd_logos.item(document.getElementsByClassName('dvd-logo').length - 1).remove()
+
+    // Checking if there is the last one logo on the page
+    if (dvd_logos.length === 1) {
         document.getElementById("dvd-remove").style.display = "none"
     }
-    dvd_logos.item(document.getElementsByClassName('dvd-logo').length-1).remove()
+
 })
 
 // Remove warning message by click
 document.addEventListener('click', () => {
     document.getElementById('warning-text').style.display = "none"
 })
+
+setTimeout(()=>{
+    document.getElementById('help-text').style.opacity = "0"
+}, 5000)
